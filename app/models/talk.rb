3be62a2250ffd4d54e0f5dde30b3d11d984a820a
Nicholas Henry <nicholas.henry@gmail.com>
2015-11-24 13:18:31 -0500

    Remove votes
    This is not a planned feature.

---
+++ app/models/talk.rb
@@ -0,0 +1,23 @@
+# == Schema Information
+#
+# Table name: talks
+#
+#  id          :integer          not null, primary key
+#  title       :string(255)
+#  description :text
+#  bio         :text
+#  slides      :string(255)
+#  author_id   :integer
+#  created_at  :datetime
+#  updated_at  :datetime
+#
+
+class Talk < ActiveRecord::Base
+  belongs_to :author, class_name: "User"
+
+  validates :author, presence: true
+
+  def bio
+    self[:bio].blank? ? author.bio : self[:bio]
+  end
+end
