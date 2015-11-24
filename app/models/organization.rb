04d47a625bdbaae96e0c34fa4af02d18feeb7647
Nicholas Henry <nicholas.henry@gmail.com>
2015-11-24 13:40:55 -0500

    Rename company to organization
    Not all entities are companies, so organizations is a better abstraction

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -1,9 +1,9 @@
 class Organization < ActiveRecord::Base
-  has_many :jobs
   translates :description
   mount_uploader :logo, LogoUploader
 
   validates :name, uniqueness: true, presence: true
 
   has_many :members
+  has_many :jobs
 end
