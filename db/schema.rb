b9eeb4ba17163d8684255828856e298184d31274
Pierre-Yves Poujade <py@poujade.org>
2015-12-05 21:10:43 -0500

    Move foreign key from Users to Members

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151204174632) do
+ActiveRecord::Schema.define(version: 20151206020220) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -77,6 +77,7 @@ ActiveRecord::Schema.define(version: 20151204174632) do
     t.text     "biography"
     t.datetime "created_at",     null: false
     t.datetime "updated_at",     null: false
+    t.integer  "user_id"
   end
 
   create_table "news_items", force: :cascade do |t|
@@ -151,4 +152,5 @@ ActiveRecord::Schema.define(version: 20151204174632) do
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   add_index "users", ["uid"], name: "index_users_on_uid", using: :btree
 
+  add_foreign_key "members", "users"
 end
